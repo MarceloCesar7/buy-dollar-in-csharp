4dcc383470aa82fdc6e672daa182394e23b2dc61
Marcelo Cesar <marcelocesar9812@gmail.com>
2023-05-10 15:48:22 -0300

    first commit

---
+++ Dolar/ConversorDeMoeda.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolar
+{
+    internal class ConversorDeMoeda
+    {
+        public double Dolar;
+        public double QuantiDolar;
+        public double Reais;
+        public double Iof;
+
+        public double Real ()
+        {
+            return Reais = Dolar * QuantiDolar;
+
+
+        }
+        
+        
+
+
+
+
+    }
+}
---
+++ Dolar/Program.cs
@@ -0,0 +1,11 @@
+﻿using Dolar;
+using System.Globalization;
+
+ConversorDeMoeda converte = new ConversorDeMoeda();
+
+Console.Write("Qual é a cotação do dolar ?");
+converte.Dolar = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+Console.Write("Quantos dolares você vai comprar ?");
+converte.QuantiDolar = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+Console.Write("Valor a ser pago em reais:" + converte.Real().ToString("F2",CultureInfo.InvariantCulture));
